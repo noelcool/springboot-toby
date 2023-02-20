cb60c04efd7ab32f090116df7104e0af197246be
noel <noelcoolblue@gmail.com>
2023-02-20 23:02:50 +0900

    프론트 컨트롤러로 전환

--- helloboot/src/main/java/noelspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/noelspring/helloboot/HellobootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
@@ -24,17 +25,25 @@ public class HellobootApplication {
 		//SpringApplication.run(HellobootApplication.class, args);
 		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
 		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
-			servletContext.addServlet("hello", new HttpServlet() {
+			servletContext.addServlet("front controller", new HttpServlet() {
+
 				@Override
 				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-					String name = req.getParameter("name");
+					// 인증, 보안, 다국어, 공통 기능 ~~
+					if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+						String name = req.getParameter("name");
+						resp.setStatus(HttpStatus.OK.value());
+						resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+						resp.getWriter().print("Hello " + name);
+					} else if (req.getRequestURI().equals("/user")) {
 
-					resp.setStatus(HttpStatus.OK.value());
-					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
-					resp.getWriter().print("Hello " + name);
+					} else {
+						resp.setStatus(HttpStatus.NOT_FOUND.value());
+					}
+
 					//super.service(req, resp);
 				}
-			}).addMapping("/hello");
+			}).addMapping("/*");
 		});
 		webServer.start();
 	}
