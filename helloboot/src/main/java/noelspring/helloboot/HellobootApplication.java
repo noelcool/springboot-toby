377f9db32b101bad94ecca4b16857591542de4e8
noel <noelcoolblue@gmail.com>
2023-02-20 22:43:07 +0900

    서블릿 요청 처리

---
+++ helloboot/src/main/java/noelspring/helloboot/HellobootApplication.java
@@ -0,0 +1,42 @@
+package noelspring.helloboot;
+
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@SpringBootApplication
+public class HellobootApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(HellobootApplication.class, args);
+		TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+		WebServer webServer = tomcatServletWebServerFactory.getWebServer(servletContext -> {
+			servletContext.addServlet("hello", new HttpServlet() {
+				@Override
+				protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+					String name = req.getParameter("name");
+
+					resp.setStatus(HttpStatus.OK.value());
+					resp.setHeader(HttpHeaders.CONTENT_TYPE, MediaType.TEXT_PLAIN_VALUE);
+					resp.getWriter().print("Hello " + name);
+					//super.service(req, resp);
+				}
+			}).addMapping("/hello");
+		});
+		webServer.start();
+	}
+
+}
