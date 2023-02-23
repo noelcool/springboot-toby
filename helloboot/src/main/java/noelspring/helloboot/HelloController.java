3ad0055a2c32ad17548f64a4d4c43a5173acb5ce
jungyin <noelcool0101@gmail.com>
2023-02-23 22:24:04 +0900

    DI를 이용한 Decorator, Proxy 패턴

--- helloboot/src/main/java/noelspring/helloboot/HelloController.java
+++ helloboot/src/main/java/noelspring/helloboot/HelloController.java
@@ -1,6 +1,5 @@
 package noelspring.helloboot;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,7 +9,6 @@ import java.util.Objects;
 public class HelloController {
 
     private final HelloService helloService;
-    // private final ApplicationContext applicationContext;
 
     public HelloController(HelloService helloService) {
         this.helloService = helloService;
--- helloboot/src/test/java/noelspring/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/noelspring/helloboot/HelloServiceTest.java
@@ -8,10 +8,14 @@ public class HelloServiceTest {
     @Test
     void simpleHelloService() {
         SimpleHelloService simpleHelloService = new SimpleHelloService();
-
         String ret = simpleHelloService.sayHello("Test");
-
         Assertions.assertThat(ret).isEqualTo("Hello Test");
+    }
 
+    @Test
+    void helloDecorator() {
+        HelloDecorator decorator = new HelloDecorator(name -> name);
+        String ret = decorator.sayHello("Test");
+        Assertions.assertThat(ret).isEqualTo("* Test *");
     }
 }
