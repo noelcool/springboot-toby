68abc4fe9e898812f304151d0e318b9cc891cd5a
jungyin <noelcool0101@gmail.com>
2023-02-23 22:04:20 +0900

    DI와 단위 테스트

--- helloboot/src/test/java/noelspring/helloboot/HelloApiTest.java
+++ helloboot/src/test/java/noelspring/helloboot/HelloApiTest.java
@@ -8,7 +8,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.client.RestTemplate;
 
 public class HelloApiTest {
 
@@ -21,6 +20,13 @@ public class HelloApiTest {
         Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
         Assertions.assertThat(res.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
         Assertions.assertThat(res.getBody()).isEqualTo("Hello Spring");
-
     }
+
+    @Test
+    void failsHelloApi() {
+        TestRestTemplate rest = new TestRestTemplate();
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+        Assertions.assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
 }
---
+++ helloboot/src/test/java/noelspring/helloboot/HelloServiceTest.java
@@ -0,0 +1,17 @@
+package noelspring.helloboot;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloServiceTest {
+
+    @Test
+    void simpleHelloService() {
+        SimpleHelloService simpleHelloService = new SimpleHelloService();
+
+        String ret = simpleHelloService.sayHello("Test");
+
+        Assertions.assertThat(ret).isEqualTo("Hello Test");
+
+    }
+}
