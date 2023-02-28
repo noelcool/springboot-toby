3ee0314d81d97e65ab0812143274850ff2409947
jungyin <noelcool0101@gmail.com>
2023-02-28 23:40:25 +0900

    스타터와 Jetty 서버 구성 추가

--- helloboot/src/main/java/noelspring/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/noelspring/config/autoconfig/JettyWebServerConfig.java
@@ -1,16 +1,16 @@
 package noelspring.config.autoconfig;
 
 import noelspring.config.MyAutoConfiguration;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
 @MyAutoConfiguration
-public class TomcatWebServerConfig {
+public class JettyWebServerConfig {
 
-    @Bean("tomcatWebServerFactory")
+    @Bean("jettyWebServerFactory")
     public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+        return new JettyServletWebServerFactory();
     }
 
 }
