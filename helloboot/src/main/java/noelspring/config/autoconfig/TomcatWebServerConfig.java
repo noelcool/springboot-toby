22252b0ad7d8f44fde039fcddcdf6f195240f9a0
jungyin <noelcool0101@gmail.com>
2023-03-01 22:58:31 +0900

    @Conditional과 Condition

--- helloboot/src/main/java/noelspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/noelspring/config/autoconfig/TomcatWebServerConfig.java
@@ -4,8 +4,13 @@ import noelspring.config.MyAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 @MyAutoConfiguration
+@Conditional(TomcatWebServerConfig.TomcatCondition.class)
 public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
@@ -13,4 +18,10 @@ public class TomcatWebServerConfig {
         return new TomcatServletWebServerFactory();
     }
 
+    static class TomcatCondition implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            return true;
+        }
+    }
 }
