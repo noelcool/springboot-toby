9940e03ba8dcbbef9067c5e5a35cf305fb34dc1e
jungyin <noelcool0101@gmail.com>
2023-03-06 20:33:04 +0900

    자동 구성에 Environment 프로퍼티 적용

--- helloboot/src/main/java/noelspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/noelspring/helloboot/HellobootApplication.java
@@ -1,7 +1,11 @@
 package noelspring.helloboot;
 
 import noelspring.config.MySpringBootAnnotation;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @MySpringBootAnnotation
 public class HellobootApplication {
@@ -9,6 +13,5 @@ public class HellobootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HellobootApplication.class, args);
 	}
-
 
 }
